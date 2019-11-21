753d043149f1a00c887ba5c2d27b977b83284823
Dalton Wais <georgedaltonwais@gmail.com>
2019-11-21 08:08:05 -0700

    Updates

---
+++ precache-manifest.d29321608ed55159beb3813982b96120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266f754923f1b3c2071cb78e4e4c9560",
+    "url": "/dalton-wais/index.html"
+  },
+  {
+    "revision": "9e2af0dc4a2bcd7b2910",
+    "url": "/dalton-wais/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5341b4c1fd3e1ca3b7a8",
+    "url": "/dalton-wais/static/js/2.082b0823.chunk.js"
+  },
+  {
+    "revision": "9e2af0dc4a2bcd7b2910",
+    "url": "/dalton-wais/static/js/main.e1e86f21.chunk.js"
+  },
+  {
+    "revision": "324b2dd34f7b6b7ca28e",
+    "url": "/dalton-wais/static/js/runtime-main.06b12f11.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dalton-wais/static/media/logo.25bf045c.svg"
+  }
+]);
