1f9113baaf13bfc413b894ec543379f68d93c380
Dalton Wais <georgedaltonwais@gmail.com>
2019-11-22 22:43:51 -0700

    updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+---
+permalink: /index.html
+---
+
 # gdwais.github.io
+
+personal resume website and blog built using React
 
--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>dalton wais</code>.
         </p>
+{/*         
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
