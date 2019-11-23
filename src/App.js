df532b13c9bdbab4522fe1a07b53d6d16e6389f2
Dalton Wais <georgedaltonwais@gmail.com>
2019-11-23 07:53:21 -0700

    recent updates"

--- src/App.js
+++ src/App.js
@@ -11,16 +11,6 @@ function App() {
         <p>
           <code>{"<"}dalton wais{">"}</code>
         </p>
-        
-{/*         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
       <footer className="App-footer">
       <p>
