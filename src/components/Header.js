89e3584da0fb0b4d396ae77530cc03a407fe9d40
Dalton Wais <dalton@fallingcolors.com>
2019-11-30 21:09:10 -0700

    adding social links

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,9 +21,17 @@ class Header extends Component {
          <div className="banner-text">
             <h3><code>{"<"} dalton wais {">"}</code></h3>
             <h4><code>Colorado based developer, consultant & curious human</code></h4>
+            <br/>
+            <br/>
+            <br/>
+            <ul className="social">
+               <li key="linkedin"><a href="https://www.linkedin.com/in/daltonwais"><i className="fa fa-linkedin"></i></a></li>
+               <li key="github"><a href="https://github.com/gdwais/daltonwais"><i className="fa fa-github"></i></a></li>
+            </ul>
          </div>
       </div>
 
+      
       <p className="scrolldown">
          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
       </p>
